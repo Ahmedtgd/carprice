6178b0bb212f47541948c05e959a613d1bf163ff
Ahmed Tarek <ahmedtarek3885@gmail.com>
2023-08-13 13:03:20 +0300

    Add files via upload
    the sebaret json data for cars

---
+++ js/data.js
@@ -0,0 +1,12 @@
+let cars = [
+    
+    { name: "Chevrolet", price: 15000, currency: "EGP" },
+    { name: "Ford", price: 17000, currency: "EGP" },
+    { name: "Dodge", price: 18000, currency: "USD" },
+    { name: "Honda", price: 19000, currency: "USD" },
+    { name: "Nissan", price: 22000, currency: "EGP" },
+    { name: "Toyota", price: 23000, currency: "USD" },
+    { name: "BMW", price: 30000, currency: "USD" },
+  ];
+  
+  module.exports = cars;
